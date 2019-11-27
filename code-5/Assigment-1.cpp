d4244c6e86a902e3d884d8bfdf2b1cbfefdab5bb
bmccBC <57549670+bmccBC@users.noreply.github.com>
2019-11-27 16:01:35 -0500

    Add files via upload

---
+++ code-5/Assigment-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+ int score;
+ int grade;
+ // Let the user enter his exam score and store it in the score variable 
+
+ // Then call the EvalauteGrade function and store the return value in 
+ //grade variable 
+
+ //Finaly display the grade variable to the user 
+
+  
+  
+  return 0;
+}
+
+char EvaluateGrade(int score)
+{
+  char grade;
+
+  // if grade from 100-90 return A
+  // if grade from 80-89 return B
+  // if grade from 70-79 return C
+  // if grade from 60-69 return D
+  // if grade less than 60 return F
+
+  return grade;
+}
